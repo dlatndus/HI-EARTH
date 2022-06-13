7136e2a20e1a32b6de6c837712f3df1cb0aa31cc
seungchae <s2125@e-mirim.hs.kr>
2022-06-13 22:08:52 +0900

    mmm

---
+++ Assets/script/Jump.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Jump : MonoBehaviour
+{
+    Touch t;
+
+    public Transform player;
+    public float jump;
+    bool isjump=false;
+    Rigidbody2D rigid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = GameObject.Find("player").GetComponent<Touch>();
+        rigid = player.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+           float thisy = player.position.y;
+        if (isjump)
+        {
+            rigid.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+            if (player.transform.position.y >= thisy) isjump = false;
+
+        }
+    }
+    public void Up()
+    {
+
+    }
+    public void Down()
+    {
+        if(t.istouch)isjump = true;
+    }
+}
---
+++ Assets/script/playerMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+    public Transform player;
+    public float Speed;
+    bool left, right;
+    // Start is called before the first frame update
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (right)
+        {
+            player.position += Vector3.right * Speed * Time.deltaTime;
+        }
+        if (left)
+        {
+            player.position += Vector3.left * Speed * Time.deltaTime;
+        }
+    }
+    public void Up()
+    {
+        right = false;
+    }
+    public void Dawn()
+    {
+        right = true;
+    }
+    public void BackUp()
+    {
+        left = false;
+    }
+    public void BackDown()
+    {
+        left = true;
+    }
+}
