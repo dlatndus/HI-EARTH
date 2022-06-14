31bee0f454667b9c226ee6be162f22b8cd7206d1
seungchae <s2125@e-mirim.hs.kr>
2022-06-15 08:38:58 +0900

    animation

--- Assets/script/Jump.cs
+++ Assets/script/Jump.cs
@@ -11,27 +11,34 @@ public class Jump : MonoBehaviour
     public float jump;
     bool isjump=false;
     Rigidbody2D rigid;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         t = GameObject.Find("player").GetComponent<Touch>();
         rigid = player.GetComponent<Rigidbody2D>();
+        animator = player.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-           float thisy = player.position.y;
+        animator.SetBool("isjump", false);
+        float thisy = player.position.y;
         if (isjump)
         {
             rigid.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
-            if (player.transform.position.y >= thisy) isjump = false;
-
+            if (player.transform.position.y >= thisy)
+            {
+                isjump = false;
+            }
         }
+        if (Mathf.Abs(rigid.velocity.y) > 0.5) animator.SetBool("isjump", true);
+        else animator.SetBool("isjump", false);
     }
     public void Up()
     {
-
+        isjump = false;
     }
     public void Down()
     {
--- Assets/script/playerMove.cs
+++ Assets/script/playerMove.cs
@@ -6,21 +6,34 @@ public class playerMove : MonoBehaviour
 {
     public Transform player;
     public float Speed;
-    bool left, right;
+    bool right,left;
+    Animator animator;
+    Rigidbody2D rigid;
+
     // Start is called before the first frame update
-    
+    void Start()
+    {
+        animator = player.GetComponent<Animator>();
+        rigid = player.GetComponent<Rigidbody2D>();
+
+    }
     // Update is called once per frame
     void Update()
     {
         if (right)
         {
             player.position += Vector3.right * Speed * Time.deltaTime;
+            player.transform.localScale = new Vector3(0.45f, 0.45f, 1f);
         }
         if (left)
         {
             player.position += Vector3.left * Speed * Time.deltaTime;
+            player.transform.localScale = new Vector3(-0.45f, 0.45f, 1f);
         }
+        if (Mathf.Abs(rigid.velocity.x) < 0.03) animator.SetBool("iswalk", false);
+        else animator.SetBool("iswalk", true);
     }
+
     public void Up()
     {
         right = false;
