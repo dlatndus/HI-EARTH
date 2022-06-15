6b7be7e39bc2a51d20affe6eee12e2e8a26d6be1
seungchae <s2125@e-mirim.hs.kr>
2022-06-16 06:02:31 +0900

    돌 클릭-> 이미지 변경

---
+++ Assets/script/Click_rock.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click_rock : MonoBehaviour
+{
+    int cnt = 0;
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprites[0];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cnt > 0)
+        {
+            spriteRenderer.sprite = sprites[cnt];
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        cnt++;
+    }
+}
