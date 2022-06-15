f406757cebbe3ff3e4d697c0e85d39c50541bac6
임수연 <81746616+dlatndus@users.noreply.github.com>
2022-06-15 15:43:56 +0900

    move filp

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -5,16 +5,57 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float jump;
-    public int speed = 10; //스피드 
+    public int speed = 5; //스피드 
+    private float horizontal;
+    private Animator animator;
+    private Rigidbody2D Rigidbody;
+    private SpriteRenderer renderer;
+
+    void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
-        float xMove = Input.GetAxis("Horizontal") * speed * Time.deltaTime; //x축으로 이동할 양
-        float yMove = Input.GetAxis("Vertical") * speed * Time.deltaTime; //y축으로 이동할양
-        this.transform.Translate(new Vector3(xMove, yMove, 0));  //이동
+        horizontal = Input.GetAxis("Horizontal");
+        PlayerMoveOnKey();
 
+        
+        
+    }
+
+
+
+    private void PlayerMoveOnKey()
+    {
+        animator.SetFloat("speed", Mathf.Abs(horizontal));
+
+        //점프
         if (Input.GetKeyDown(KeyCode.Space))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
         }
+
+        if (horizontal < 0)
+        {
+            renderer.flipX = true;
+        }
+        else
+        {
+            renderer.flipX = false;
+        }
+        Rigidbody.velocity = new Vector2(horizontal * speed, Rigidbody.velocity.y);
+
+        /*
+        float xMove = Input.GetAxis("Horizontal") * speed * Time.deltaTime; //x축으로 이동할 양
+        float yMove = Input.GetAxis("Vertical") * speed * Time.deltaTime; //y축으로 이동할양
+        this.transform.Translate(new Vector3(xMove, yMove, 0));  //이동
+        
+
+        if(horisontal)
+        */
     }
 }
